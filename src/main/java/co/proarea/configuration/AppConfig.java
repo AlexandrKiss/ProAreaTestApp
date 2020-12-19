044482403d66933a3c26529c736cd889f1a57655
alexandr_kiss <al.kisurin@gmail.com>
2020-12-19 13:58:50 +0200

    AppConfig - off

--- src/main/java/co/proarea/configuration/AppConfig.java
+++ src/main/java/co/proarea/configuration/AppConfig.java
@@ -16,43 +16,43 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @Configuration
 @EnableScheduling
 public class AppConfig {
-    Product burger = new Product("Burger",1L);
-    ProductUnit bigMac = new ProductUnit("Big Mac", 1L, 4.35,burger);
-    ProductUnit quarterPounder = new ProductUnit("Quarter Pounder", 2L, 4.40,burger);
-    ProductUnit quarterPounderDeluxe = new ProductUnit("Quarter Pounder Deluxe", 3L, 5.0,burger);
+//    Product burger = new Product("Burger",1L);
+//    ProductUnit bigMac = new ProductUnit("Big Mac", 1L, 4.35,burger);
+//    ProductUnit quarterPounder = new ProductUnit("Quarter Pounder", 2L, 4.40,burger);
+//    ProductUnit quarterPounderDeluxe = new ProductUnit("Quarter Pounder Deluxe", 3L, 5.0,burger);
+//
+//    Product chicken = new Product("Chicken",2L);
+//    ProductUnit buttermilkCrispyChicken = new ProductUnit("Buttermilk Crispy Chicken", 4L, 5.0,chicken);
+//    ProductUnit artisanGrilledChicken = new ProductUnit("Artisan Grilled Chicken", 5L, 5.0,chicken);
+//
+//    Role userRole = new Role("ROLE_USER");
+//    Role adminRole = new Role("ROLE_ADMIN");
+//
+//    User admin = new User("admin","Admin","Admin", "al.kisurin@gmail.com","admin");
+//    User user = new User("user","FirstName","LastName", "user@mail.com","user");
+//    User user2 = new User("user2","FirstName2","LastName2", "user2@mail.com","user");
 
-    Product chicken = new Product("Chicken",2L);
-    ProductUnit buttermilkCrispyChicken = new ProductUnit("Buttermilk Crispy Chicken", 4L, 5.0,chicken);
-    ProductUnit artisanGrilledChicken = new ProductUnit("Artisan Grilled Chicken", 5L, 5.0,chicken);
-
-    Role userRole = new Role("ROLE_USER");
-    Role adminRole = new Role("ROLE_ADMIN");
-
-    User admin = new User("admin","Admin","Admin", "al.kisurin@gmail.com","admin");
-    User user = new User("user","FirstName","LastName", "user@mail.com","user");
-    User user2 = new User("user2","FirstName2","LastName2", "user2@mail.com","user");
-
-    @Bean
-    public CommandLineRunner demo(final ProductService productUnitService, final ProductService productService,
-                                  final UserService userService) {
-        return new CommandLineRunner() {
-            @Override
-            public void run(String... strings) throws Exception {
-                productService.addProduct(burger);
-                productService.addProduct(chicken);
-                productUnitService.addProductUnit(new ProductUnitDTO(bigMac));
-                productUnitService.addProductUnit(new ProductUnitDTO(quarterPounder));
-                productUnitService.addProductUnit(new ProductUnitDTO(quarterPounderDeluxe));
-                productUnitService.addProductUnit(new ProductUnitDTO(buttermilkCrispyChicken));
-                productUnitService.addProductUnit(new ProductUnitDTO(artisanGrilledChicken));
-
-                userService.addRole(adminRole);
-                userService.addRole(userRole);
-
-                userService.register(admin);
-                userService.register(user);
-                userService.register(user2);
-            }
-        };
-    }
+//    @Bean
+//    public CommandLineRunner demo(final ProductService productUnitService, final ProductService productService,
+//                                  final UserService userService) {
+//        return new CommandLineRunner() {
+//            @Override
+//            public void run(String... strings) throws Exception {
+//                productService.addProduct(burger);
+//                productService.addProduct(chicken);
+//                productUnitService.addProductUnit(new ProductUnitDTO(bigMac));
+//                productUnitService.addProductUnit(new ProductUnitDTO(quarterPounder));
+//                productUnitService.addProductUnit(new ProductUnitDTO(quarterPounderDeluxe));
+//                productUnitService.addProductUnit(new ProductUnitDTO(buttermilkCrispyChicken));
+//                productUnitService.addProductUnit(new ProductUnitDTO(artisanGrilledChicken));
+//
+//                userService.addRole(adminRole);
+//                userService.addRole(userRole);
+//
+//                userService.register(admin);
+//                userService.register(user);
+//                userService.register(user2);
+//            }
+//        };
+//    }
 }
